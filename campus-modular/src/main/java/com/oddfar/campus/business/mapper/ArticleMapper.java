13b4271de8f0340960ec85398e36641b66a23366
JCXYOZH <2729028597@qq.com>
2025-04-08 11:38:37 +0800

    second push

---
+++ campus-modular/src/main/java/com/oddfar/campus/business/mapper/ArticleMapper.java
@@ -0,0 +1,23 @@
+package com.oddfar.campus.business.mapper;
+
+import com.oddfar.campus.business.domain.Article;
+import com.oddfar.campus.common.core.BaseMapperX;
+import com.oddfar.campus.common.core.LambdaQueryWrapperX;
+import com.oddfar.campus.common.domain.PageResult;
+
+/**
+ * 文章管理Mapper
+ */
+public interface ArticleMapper extends BaseMapperX<Article> {
+
+    default PageResult<Article> page(Article article) {
+
+        return selectPage(new LambdaQueryWrapperX<Article>()
+                .likeIfPresent(Article::getArticleTitle, article.getArticleTitle())
+                .likeIfPresent(Article::getArticleContent, article.getArticleContent())
+                .eqIfPresent(Article::getIsAnonymous, article.getIsAnonymous())
+                .betweenIfPresent(Article::getCreateTime,article.getParams())
+        );
+    }
+
+}
